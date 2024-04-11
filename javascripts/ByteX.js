c7fa8fca342b6c878659db7ab9747437fa2db689
1337encrypted <smallaszero@gmail.com>
2024-04-11 15:07:13 +0530

    refactored

--- javascripts/ByteX.js
+++ javascripts/ByteX.js
@@ -1,32 +1,26 @@
 var styleElement = document.createElement('style');
 
-var styleElement = document.createElement('style');
-
-styleElement.textContent = cssText;
-
-var cssText = {
-
-}
-
 // Append the <style> element to the <head> section of the document
 document.head.appendChild(styleElement);
 // Object to store text contents and their corresponding IDs
 var textContents = {
-    'text1': '<h3> Level 0x00</h3> <p>Welcome to level zero of the BYTEX challenges!<br>In this level, you will learn how to set up the arduino-cli software for the Arduino UNO board.<br><h5>Installation Steps:</h5>Execute the installation script with the following command:<br>"./install_arduino_cli.sh"<br>If you are unsure about the type of board you are using, connect it to the Raspberry Pi via USB and run the command below to check:<br>"arduino-cli board list"<br><h5>Expected Output:</h5><br>"Port         Protocol Type              Board Name  FQBN Core/dev/ttyACM0 serial   Serial Port (USB) Arduino Uno arduino:avr:uno arduino:avr"<br>Note: /dev/ttyACM0 is your port number. Remember this for future code uploads.<br>To confirm successful installation, enter arduino-cli in the terminal. You should receive some output. If not, your installation may not have been successful.<br><h5>Password Retrieval:</h5><br>Try executing the "password.sh" file to obtain the password for the next level:</p>'  ,
-    'text2': '<h3> Level 0x01</h3> <p>Congratulations on reaching level 1!<br>In this level, you will learn how to make an LED blink using arduino-cli.<br>Generate your first sketch, named Blink, using the following command:<br>"arduino-cli sketch new Blink"<br>Open the nano editor to modify the code:<br>"nano Blink/Blink.ino"<br>Once inside the editor, insert the classic blink code provided below:<br>"void setup() {<br>pinMode(LED_BUILTIN, OUTPUT);<br>}<br>void loop() {<br>digitalWrite(LED_BUILTIN, HIGH);<br>delay(1000);<br>digitalWrite(LED_BUILTIN, LOW);<br>delay(1000);<br>}"<br<<br>Save the changes by pressing Ctrl+O and then exit with Ctrl+X.<br>To compile, use the following syntax:<br>"arduino-cli compile --fqbn arduino:board_name Blink/"<br>For an Arduino UNO, it would be:<br>"arduino-cli compile --fqbn arduino:avr:uno Blink/"<br>To upload the compiled sketch:<br>"arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno Blink/" </p>',
-    'text3': '<h3> Level 0x02</h3><p></p>',
-    'text4': '<h3> Level 0x03</h3> <p></p>',
-    'text5': '<h3> Level 0x04</h3><p></p>',
-    'text6': '<h3> Level 0x05</h3><p></p>',
-    'text7': '<h3> Level 0x06</h3><p></p>',
-    'text8': '<h3> Level 0x07</h3><p></p>',
-    'text9': '<h3> Level 0x08</h3><p></p>',
-    'text10': '<h3> Level 0x09</h3><p></p>',
-    'text11': '<h3> Level 0x10</h3><p></p>',
+    'Level 0x00': ``,
+    'Level 0x01': ``,
+    'Level 0x02': ``,
+    'Level 0x03': ``,
+    'Level 0x04': ``,
+    'Level 0x05': ``,
+    'Level 0x06': ``,
+    'Level 0x07': ``,
+    'Level 0x08': ``,
+    'Level 0x09': ``,
+    'Level 0x0A': ``,
 };
 
-// JavaScript function to display text based on clicked link
 function displayText(contentId) {
+    var articleHeaderBottom = document.querySelector(".articleHeaderBottom");
+    articleHeaderBottom.innerHTML = contentId;
+
     var textBox = document.getElementById("textBox");
     textBox.innerHTML = textContents[contentId] || 'Text not found!';
 }
--- javascripts/Shellx.js
+++ javascripts/Shellx.js
@@ -1,42 +1,106 @@
 var styleElement = document.createElement('style');
 
-var styleElement = document.createElement('style');
+// styleElement.textContent = cssText;
 
-styleElement.textContent = cssText;
+// var cssText = {
 
-var cssText = {
-
-}
+// }
 
 // Append the <style> element to the <head> section of the document
 document.head.appendChild(styleElement);
 // Object to store text contents and their corresponding IDs
 var textContents = {
-    'text1': '<h3> Level 0x00</h3> <p class="levelContent">The objective of this stage is to access the game via SSH. You should connect to the host bandit.labs.overthewire.org using port 2220, with the credentials bandit0 (username) and bandit0 (password). After successfully logging in, visit the Level 1 section to learn how to progress to the next level. <br> Commands you may need to solve this level <br> <a href="https://www.geeksforgeeks.org/ssh-command-in-linux-with-examples/">ssh</a> </p>'  ,
+'Level 0x00':`
+The objective of this stage is to access the game via SSH.
+You should connect to the host whose IP is displayed on the LCD screen on the IOT evaluation board.
 
-    'text2': '<h3> Level 0x01</h3> <p> The passphrase for accessing the subsequent stage can be found within a document named readme in the main folder. Utilize this passphrase for accessing Level 0x02 via SSH. Each time you unearth a passphrase for a stage, employ SSH (using port 2220) to gain entry to that stage and proceed with the challenge.<br>Commands potentially useful for navigating this stage include <br> <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> , <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a><a href="https://www.geeksforgeeks.org/du-command-linux/">du</a><a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.</p>',
+Use the credentials:
+Username: <strong>shell00</strong>
+Password: <strong>shell00</strong>
 
-    'text3': '<h3> Level 0x02</h3><p>The passphrase for advancing to the subsequent level can be found within a document named "-" located in the main directory.<br>Commands that might be useful for completing this level include<br> <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> , <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a><a href="https://www.geeksforgeeks.org/du-command-linux/">du</a><a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>. <br> Hint:<br>Perform a Google search for "dashed filename".</p>',
+After successfully logging in, visit the Level 1 section to learn how to progress to the next level.
 
-    'text4': '<h3> Level 0x03</h3> <p>The passphrase for accessing the next level can be found within a hidden file within the inhere directory.<br>Commands potentially useful for navigating this stage include <br> <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> , <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a><a href="https://www.geeksforgeeks.org/du-command-linux/">du</a><a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.</p>',
+<h3>Commands you may need to solve this level</h3>
+<a href="https://www.youtube.com/watch?v=_3M7Y0iC7PE" target="_blank">ssh</a>`,
 
-    'text5': '<h3> Level 0x04</h3><p>The passphrase for advancing to the subsequent level is located in the sole file that is readable by humans within the inhere directory. Hint: should your terminal display errors, consider using the "reset" command.<br>Commands that might be beneficial for completing this level include<br><a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> , <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a><a href="https://www.geeksforgeeks.org/du-command-linux/">du</a><a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.</p>',
+'Level 0x01':`
+The passphrase for accessing the subsequent stage can be found within a document named <strong>readme</strong>
+in the main folder.
+Utilize this passphrase for accessing Level 0x02 via SSH.
 
-    'text6': '<h3> Level 0x05</h3><p>The passphrase for progressing to the next level can be found in a file within the inhere directory that possesses the following characteristics:<br>- Readable by humans<br>- Size of 1033 bytes<br>- Not set as executable<br>Commands that might be beneficial for completing this level include<br><a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> , <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a><a href="https://www.geeksforgeeks.org/du-command-linux/">du</a><a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.</p>',
+<h3>Commands potentially useful for navigating this stage include</h3>
+<a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>, <a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a>, <a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a>, <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>`,
 
-    'text7': '<h3> Level 0x06</h3><p>The passphrase for accessing the subsequent level is located somewhere on the server and meets the following criteria:<br>- Owned by user bandit7<br>- Belongs to group Level0x05<br></br>- Size of 33 bytes<br>Commands that might be beneficial for completing this level include<br><a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> , <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a><a href="https://www.geeksforgeeks.org/du-command-linux/">du</a><a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a><a href="https://www.geeksforgeeks.org/grep-command-in-unixlinux/">grep</a>.</p>',
+'Level 0x02':`
+The passphrase for advancing to the subsequent level can be found within a document named <strong>"-"</strong>
+located in the main directory.
 
-    'text8': '<h3> Level 0x07</h3><p>The passphrase for advancing to the next level is located in the file data.txt next to the term "millionth."<br>Commands that might be useful for solving this level include:<br><a href="https://www.geeksforgeeks.org/man-command-in-linux-with-examples/">man</a>, <a href="https://www.geeksforgeeks.org/grep-command-in-unixlinux/">grep</a>,<a href="https://www.geeksforgeeks.org/sort-command-linuxunix-examples/">sort</a> ,<a href="https://www.geeksforgeeks.org/uniq-command-in-linux-with-examples-2/">uniq</a> ,<a href="https://www.geeksforgeeks.org/string-manipulation-in-shell-scripting/">strings</a> , <a href="https://www.geeksforgeeks.org/convert-text-file-strings-into-base64-encoding/">base64</a>,<a href="">tr</a> , tar, gzip, bzip2, xxd.</p>',
+<h3>Commands that might be useful for completing this level include</h3>
+<a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>, <a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a>, <a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a>, <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.
 
-    'text9': '<h3> Level 0x08</h3>',
 
-    'text10': '<h3> Level 0x09</h3>',
+<p id=hint>Hint: Perform a Google search for "dashed filename".</p>`,
 
-    'text11': '<h3> Level 0x010</h3>',
+'Level 0x03':`
+The passphrase for accessing the next level can be found within a hidden file within the <strong>inhere</strong> directory.
+
+<h3>Commands potentially useful for navigating this stage include</h3>
+<a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>, <a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a>, <a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a>, <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.`,
+
+'Level 0x04':`
+The passphrase for advancing to the subsequent level is located in the sole file that is readable by
+humans within the <strong>inhere</strong> directory.
+
+<h3>Commands that might be beneficial for completing this level include</h3>
+<a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a> ,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> ,<a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a> ,<a href="https://www.geeksforgeeks.org/du-command-linux/">du</a> ,<a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.
+
+<p id=hint>Hint: should your terminal display errors, consider using the "reset" command.</p>`,
+
+'Level 0x05':`
+The passphrase for progressing to the next level can be found in a file within the <strong>inhere</strong> directory
+that possesses the following characteristics:
+- Readable by humans
+- Size of 1033 bytes
+- Not set as executable
+
+<h3>Commands that might be beneficial for completing this level include</h3>
+<a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a> ,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> ,<a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.`,
+
+'Level 0x06': `
+The passphrase for accessing the subsequent level is located somewhere on the server and meets the 
+following criteria:
+- Owned by user shell07
+- Belongs to group Level0x05
+- Size of 33 bytes
+
+<h3>Commands that might be beneficial for completing this level include</h3>
+<a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a> ,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> ,<a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>, <a href="https://www.geeksforgeeks.org/grep-command-in-unixlinux/">grep</a>.`,
+
+'Level 0x07':`
+The passphrase for advancing to the next level is located in the file data.txt next to the term <strong>millionth</strong>
+
+<h3>Commands that might be useful for solving this level include:</h3>
+<a href="https://www.geeksforgeeks.org/man-command-in-linux-with-examples/">man</a> ,<a href="https://www.geeksforgeeks.org/grep-command-in-unixlinux/">grep</a> ,<a href="https://www.geeksforgeeks.org/sort-command-linuxunix-examples/">sort</a>, <a href="https://www.geeksforgeeks.org/uniq-command-in-linux-with-examples-2/">uniq</a>, <a href="https://www.geeksforgeeks.org/string-manipulation-in-shell-scripting/">strings</a>, <a href="https://www.geeksforgeeks.org/convert-text-file-strings-into-base64-encoding/">base64</a>, <a href="#">tr</a> , tar, gzip, bzip2, xxd.`,
+
+'Level 0x08': '',
+'Level 0x09': '',
+'Level 0x0A': '',
 };
+
 
 // JavaScript function to display text based on clicked link
 function displayText(contentId) {
+    var articleHeaderBottom = document.querySelector(".articleHeaderBottom");
+    articleHeaderBottom.innerHTML = contentId;
+
     var textBox = document.getElementById("textBox");
     textBox.innerHTML = textContents[contentId] || 'Text not found!';
+
+    // var trIndex = parseInt(contentId.substring(contentId.length - 1));
+    // var table = document.querySelector('.level');           // Select the table   
+    // var trElement = table.querySelectorAll('tr')[trIndex - 1]; 
+
+    // // Change background color to black and text color to white
+    // trElement.style.backgroundColor = "black";
+    // trElement.style.color = "white";
 }
