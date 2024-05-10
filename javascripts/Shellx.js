585afd34b24aa7ff5a1f7b567192bbc7e906b6ae
1337encrypted <smallaszero@gmail.com>
2024-05-10 23:21:24 +0530

    added the shellx challenges

--- javascripts/Shellx.js
+++ javascripts/Shellx.js
@@ -12,14 +12,14 @@ document.head.appendChild(styleElement);
 var textContents = {
 '0x00':`
 <div class="container">
-The objective of this stage is to access the game via SSH.
-You should connect to the host whose IP is displayed on the LCD screen on the IOT evaluation board.
+<span class="text">The objective of this stage is to access the game via SSH. You should connect to the host whose IP is displayed on the LCD screen on the IOT evaluation board.</span>
 
-Use the credentials:
-Username: <strong>shell00</strong>
-Password: <strong>shell00</strong>
+<span class="text">Use the credentials:</span>
 
-After successfully logging in, visit the 1 section to learn how to progress to the next level.
+<span class="code">Username: <strong>shell00</strong>
+Password: <strong>shell00</strong></span>
+
+<span class="text">After successfully logging in, visit the 1 section to learn how to progress to the next level.</span>
 
 <h3>Commands you may need to solve this level</h3>
 <a href="https://www.youtube.com/watch?v=_3M7Y0iC7PE" target="_blank">ssh</a>
@@ -28,10 +28,11 @@ After successfully logging in, visit the 1 section to learn how to progress to t
 
 '0x01':`
 <div class="container">
+<span class="text">
 The passphrase for accessing the subsequent stage can be found within a document named <strong>readme</strong>
 in the main folder.
 Utilize this passphrase for accessing 0x02 via SSH.
-
+</span>
 <h3>Commands potentially useful for navigating this stage include</h3>
 <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>, <a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a>, <a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a>, <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>
 </div>
@@ -39,9 +40,10 @@ Utilize this passphrase for accessing 0x02 via SSH.
 
 '0x02':`
 <div class="container">
+<span class="text">
 The passphrase for advancing to the subsequent can be found within a document named <strong>"-"</strong>
 located in the main directory.
-
+<span>
 <h3>Commands that might be useful for completing this include</h3>
 <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>, <a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a>, <a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a>, <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.
 
@@ -52,8 +54,9 @@ located in the main directory.
 
 '0x03':`
 <div class="container">
+<span class="text">
 The passphrase for accessing the next can be found within a hidden file within the <strong>inhere</strong> directory.
-
+<span>
 <h3>Commands potentially useful for navigating this stage include</h3>
 <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a>, <a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a>, <a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a>, <a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.
 </div>
@@ -61,9 +64,10 @@ The passphrase for accessing the next can be found within a hidden file within t
 
 '0x04':`
 <div class="container">
+<span class="text">
 The passphrase for advancing to the subsequent is located in the sole file that is readable by
 humans within the <strong>inhere</strong> directory.
-
+<span>
 <h3>Commands that might be beneficial for completing this include</h3>
 <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a> ,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> ,<a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a> ,<a href="https://www.geeksforgeeks.org/du-command-linux/">du</a> ,<a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.
 
@@ -73,12 +77,13 @@ humans within the <strong>inhere</strong> directory.
 
 '0x05':`
 <div class="container">
+<span class="text">
 The passphrase for progressing to the next can be found in a file within the <strong>inhere</strong> directory
 that possesses the following characteristics:
-- Readable by humans
-- Size of 1033 bytes
-- Not set as executable
-
+<span>  
+<li>Readable by humans</li>
+<li>Size of 1033 bytes</li>
+<li>Not set as executable</li>
 <h3>Commands that might be beneficial for completing this include</h3>
 <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a> ,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> ,<a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>.
 </div>
@@ -86,11 +91,13 @@ that possesses the following characteristics:
 
 '0x06': `
 <div class="container">
+<span class="text">
 The passphrase for accessing the subsequent is located somewhere on the server and meets the 
 following criteria:
-- Owned by user shell07
-- Belongs to group Level0x05
-- Size of 33 bytes
+</span>
+<li>Owned by user shell07</li>
+<li>Belongs to group Level0x05</li>
+<li>Size of 33 bytes</li>
 
 <h3>Commands that might be beneficial for completing this include</h3>
 <a href="https://www.geeksforgeeks.org/ls-command-in-linux/">ls</a> ,<a href="https://www.geeksforgeeks.org/cd-command-in-linux-with-examples/">cd</a> ,<a href="https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/">cat</a> ,<a href="https://www.geeksforgeeks.org/file-command-in-linux-with-examples/">file</a>, <a href="https://www.geeksforgeeks.org/du-command-linux/">du</a>, <a href="https://www.geeksforgeeks.org/find-command-in-linux-with-examples/">find</a>, <a href="https://www.geeksforgeeks.org/grep-command-in-unixlinux/">grep</a>.
@@ -99,8 +106,9 @@ following criteria:
 
 '0x07':`
 <div class="container">
+<span class="text">
 The passphrase for advancing to the next is located in the file data.txt next to the term <strong>millionth</strong>
-
+<span>
 <h3>Commands that might be useful for solving this include:</h3>
 <a href="https://www.geeksforgeeks.org/man-command-in-linux-with-examples/">man</a> ,<a href="https://www.geeksforgeeks.org/grep-command-in-unixlinux/">grep</a> ,<a href="https://www.geeksforgeeks.org/sort-command-linuxunix-examples/">sort</a>, <a href="https://www.geeksforgeeks.org/uniq-command-in-linux-with-examples-2/">uniq</a>, <a href="https://www.geeksforgeeks.org/string-manipulation-in-shell-scripting/">strings</a>, <a href="https://www.geeksforgeeks.org/convert-text-file-strings-into-base64-encoding/">base64</a>, <a href="#">tr</a> , <a href="#">tar</a>, <a href="#">gzip</a>, <a href="#">bzip2</a>, <a href="#">xxd</a>.
 </div>
@@ -118,13 +126,5 @@ function displayText(contentId) {
     articleHeaderBottom.innerHTML = contentId;
 
     var textBox = document.getElementById("textBox");
-    textBox.innerHTML = textContents[contentId] || `<center><img src="../images/lock.png" alt="🔒"><center>`;
-
-    // var trIndex = parseInt(contentId.substring(contentId.length - 1));
-    // var table = document.querySelector('.level');           // Select the table   
-    // var trElement = table.querySelectorAll('tr')[trIndex - 1]; 
-
-    // // Change background color to black and text color to white
-    // trElement.style.backgroundColor = "black";
-    // trElement.style.color = "white";
+    textBox.innerHTML = textContents[contentId] || `<center><img id="lockimage" src="../images/lock.png" alt="🔒"><center>`;
 }
